b667bdbeca1c08214c10d00bb25d5a52ab23f35a
Alex Liang <u9211011@gmail.com>
2015-10-20 22:56:59 +0800

    Allow user add and delete aritcle

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,15 @@
 class PostsController < ApplicationController
+  before_action :find_group
+
   def new
-    @group = Group.find(params[:group_id])
     @post = @group.posts.new
   end
 
   def edit
+    @post = @group.posts.find(params[:id])
   end
 
   def create
-    @group = Group.find(params[:group_id])
     @post = @group.posts.build(post_params)
 
     if @post.save
@@ -19,15 +20,30 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = @group.posts.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to group_path(@group), notice: "文章修改成功"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @post = @group.posts.find(params[:id])
+
+    @post.destroy
+    redirect_to group_path(@group), alert: "文章己刪除!"
   end
 
   private
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def find_group
+    @group = Group.find(params[:group_id])
   end
 
 end
